a2d3806bc4516b2eea10ac079ab1268d524a9dea
tmrconfiabilidade-commits <tmr.confiabilidade@gmail.com>
2026-05-25 11:37:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,11 @@ header {visibility: hidden;}
 footer {visibility: hidden;}
 button[kind="header"] {display: none;}
 
+footer {visibility: hidden;}
+[data-testid="stStatusWidget"] {display: none;}
+[data-testid="stDecoration"] {display: none;}
+[data-testid="stToolbar"] {display: none;}
+
 .block-container {
     padding-top: 1rem;
 }
